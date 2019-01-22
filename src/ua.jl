d714283d0b256b6483020449595368e8e05e11fa
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-01-22 13:48:06 +0100

    Update ua.jl

--- src/ua.jl
+++ src/ua.jl
@@ -1,4 +1,5 @@
 import FFTW
+using LinearAlgebra
 
 export UA
 
@@ -8,7 +9,7 @@ mutable struct UA
     ε    :: Float64
     tau  :: Vector{Float64}
     ltau :: Vector{Float64}
-    ftau :: Vector{ComplexF64}
+    ftau :: Array{ComplexF64,1}
     ptau :: FFTW.cFFTWPlan{ComplexF64,-1,false,1}
 
     function UA( ntau, ε )
@@ -28,5 +29,80 @@ mutable struct UA
         new( ntau, ε, tau, ltau, ftau, ptau )
 
     end
+
+end
+
+export update_particles_e!
+
+function update_particles_e!( particles :: Particles, 
+                              et        :: Array{Float64,3}, 
+                              fields    :: MeshFields, 
+                              ua        :: UA, 
+                              xt        :: Array{ComplexF64,3}) 
+    for n=1:ua.ntau
+    
+        for m=1:particles.nbpart
+            xt1 = real(xt[n,1,m])
+            xt2 = real(xt[n,2,m])
+            particles.x[1,m] = xt1
+            particles.x[2,m] = xt2
+        end
+        
+        interpol_eb_m6!( particles, fields )
+        
+        for m=1:particles.nbpart
+            et[1,m,n] = particles.e[1,m]
+            et[2,m,n] = particles.e[2,m]
+        end
+    
+    end
+    
+end
+
+export update_particles_x!
+
+function update_particles_x!( particles :: Particles, 
+                              fields    :: MeshFields, 
+                              ua        :: UA, 
+                              xt        :: Array{ComplexF64,3}) 
+
+    nbpart = particles.nbpart
+    nx     = fields.mesh.nx
+    ny     = fields.mesh.ny
+    dx     = fields.mesh.dx
+    dy     = fields.mesh.dy
+    xmin   = fields.mesh.xmin
+    xmax   = fields.mesh.xmax 
+    ymin   = fields.mesh.ymin
+    ymax   = fields.mesh.ymax
+    dimx   = xmax - xmin
+    dimy   = ymax - ymin
+
+    for m = 1:nbpart
+
+        t = particles.t[m]
+
+        mul!(ua.ftau, ua.ptau, view(xt,:,1,m))
+
+        for n = 1:ua.ntau
+            ua.ftau[n] *= exp(1im*ua.ltau[n]*t/ua.ε)/ua.ntau
+        end 
+
+        xt1 = real(sum(ua.ftau))
+
+        mul!(ua.ftau, ua.ptau, view(xt,:,2,m))
+
+        for n = 1:ua.ntau
+            ua.ftau[n] *= exp(1im*ua.ltau[n]*t/ua.ε)/ua.ntau
+        end 
+
+        xt2 = real(sum(ua.ftau))
+
+        particles.x[1,m] = xt1
+        particles.x[2,m] = xt2
+
+    end
+
+    calcul_rho_m6!( fields, particles )
 
 end
