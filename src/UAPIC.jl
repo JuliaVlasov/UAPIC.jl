edf874a065f93752b31789f4d9e30fcf2949703d
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-01-22 13:48:09 +0100

    Update UAPIC.jl

--- src/UAPIC.jl
+++ src/UAPIC.jl
@@ -1,10 +1,10 @@
 module UAPIC
 
-include("ua.jl")
 include("meshfields.jl")
 include("integrate.jl")
 include("gnuplot.jl")
 include("poisson.jl")
 include("particles.jl")
+include("ua.jl")
 
 end # module
