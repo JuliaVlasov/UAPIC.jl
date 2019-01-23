6ae13dd79a7d142d7f416025261fea4ef1ba64f0
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-01-23 14:04:59 +0100

    Update ua_type.jl

--- src/ua_type.jl
+++ src/ua_type.jl
@@ -10,9 +10,6 @@ mutable struct UA
     ptau :: FFTW.cFFTWPlan{ComplexF64,-1,false,1}
     pl   :: Array{ComplexF64,2}
     ql   :: Array{ComplexF64,2}
-    r    :: Array{ComplexF64,2}
-    r̃    :: Array{ComplexF64,2}
-    rtau :: FFTW.cFFTWPlan{ComplexF64,-1,false,2}
 
     function UA( ntau, ε, nbpart )
 
@@ -31,12 +28,7 @@ mutable struct UA
         pl = zeros(ComplexF64, (ntau, nbpart))
         ql = zeros(ComplexF64, (ntau, nbpart))
 
-        r  = zeros(ComplexF64, (ntau,2))
-        r̃  = zeros(ComplexF64, (ntau,2))
-
-        rtau = plan_fft(r,1)
-
-        new( ntau, ε, tau, ltau, ftau, ptau, pl, ql, r, r̃, rtau )
+        new( ntau, ε, tau, ltau, ftau, ptau, pl, ql )
 
     end
 
