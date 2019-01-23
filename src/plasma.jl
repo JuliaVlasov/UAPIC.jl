a4137c8786e91f6dec114ebb46a07d1af51ece00
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-01-23 14:04:15 +0100

    Update plasma.jl

--- src/plasma.jl
+++ src/plasma.jl
@@ -9,24 +9,22 @@ function plasma( mesh :: Mesh, nbpart :: Int64 )
     dx, dy = mesh.dx, mesh.dy
     dimx = mesh.xmax - mesh.xmin
     dimy = mesh.ymax - mesh.ymin
-    
+
     weight = (dimx * dimy) / nbpart
 
     particles = Particles(nbpart, weight )
-    
+
     k = 1
     while (k<=nbpart)
 
         xi   = rand() * dimx
-        yi   = rand() * dimy 
+        yi   = rand() * dimy
         zi   = (2.0 + alpha) * rand()
         temm = 1.0 + sin(yi) + alpha * cos(kx*xi)
 
         if (temm >= zi)
-            particles.ix[k] = trunc(Int32, xi/dimx*nx)
-            particles.iy[k] = trunc(Int32, yi/dimy*ny)
-            particles.dx[k] = Float32(xi/dx - particles.ix[k])
-            particles.dy[k] = Float32(yi/dy - particles.iy[k])
+            particles.x[1,k] = xi
+            particles.x[2,k] = yi
             k = k + 1
         end
 
@@ -42,8 +40,8 @@ function plasma( mesh :: Mesh, nbpart :: Int64 )
                + exp(-((xi+2)^2 + yi^2)/2))/2
 
         if (temm >= zi)
-            particles.vx[k] = xi
-            particles.vy[k] = yi
+            particles.v[1,k] = xi
+            particles.v[2,k] = yi
             k = k + 1
         end
 
@@ -51,4 +49,4 @@ function plasma( mesh :: Mesh, nbpart :: Int64 )
 
     particles
 
-end 
+end
